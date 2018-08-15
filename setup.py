3a47152443fb1359419d676f96cae70b2e8c7f49
Ben Hurst <BHurst8@gmail.com>
2018-08-14 21:52:10 -0700

    Completed array_shift

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name='python_data_structures_and_algorithms',
+  version='0.0.1'
+  packages=['python-data-structures-and-algorithms',
+            'python-data-structures-and-algorithms.challenges',
+            'python-data-structures-and-algorithms.challenges.array_shift',
+  ],
+  url='https://github.com/bebenbuhben/python-data-structures-and-algorithms',
+  license='MIT',
+  author='Ben Hurst',
+  author_email='bhurst8@gmail.com',
+  description='Data Structures and Algorithms built using the standard Python Library'
+)
